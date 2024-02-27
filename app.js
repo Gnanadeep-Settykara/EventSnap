de225f40dd038cdbeaa810887b025d276daa8002
Gnanadeep-Settykara <gsettyka@asu.edu>
2024-02-26 22:31:09 -0700

    updated files heirarchy

--- app.js
+++ app.js
@@ -1,132 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlHTTP } = require('express-graphql');
-const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
-const Event = require('./models/event');
-const User = require('./models/user');
+const graphQlSchema = require('./graphql/schema/index');
+const graphQlResolvers = require('./graphql/resolvers/index');
 
 const app = express();
 
 app.use(bodyParser.json());
 
+
+
 app.use('/graphql', 
 graphqlHTTP({
-    schema: buildSchema(`
-      type Event {
-        _id: ID!
-        title: String!
-        description: String!
-        price: Float!
-        date: String!
-      }
-
-      type User {
-        _id: ID!
-        email: String!
-        password: String
-      }
-
-      input EventInput {
-        title: String!
-        description: String!
-        price: Float!
-        date: String!
-      }
-
-      input UserInput {
-        email: String!
-        password: String!
-      }
-      
-      type RootQuery {
-        events: [Event!]!
-      }  
-      type RootMutation {
-        createEvent(eventInput: EventInput): Event
-        createUser(userInput: UserInput): User
-      }
-
-      schema {
-        query: RootQuery
-        mutation: RootMutation
-      }
-    `),
+    schema: graphQlSchema,
     // rootValue is the bundle of resolvers
-    rootValue: {
-        events: () => {
-            return Event.find()
-            .then(events => {
-                return events.map(event => {
-                    return {...event._doc, _id: event.id};
-                });
-            })
-            .catch(err => {
-                throw err;
-            })
-        },
-        createEvent: (args) => {
-                
-            const event = new Event({
-                title: args.eventInput.title,
-                description: args.eventInput.description,
-                price: +args.eventInput.price,
-                date: new Date(args.eventInput.date),
-                creator: '65d196e4a1be1788bab10e42'
-            });
-            let createdEvent;
-            return event.save()
-            .then(result => {
-                createdEvent = {...result._doc,_id: result._doc._id.toString()};
-                 return User.findById('65d196e4a1be1788bab10e42')
-                
-            })
-            .then(user => {
-                if (!user) {
-                    throw new Error('User not Found.')
-                }
-                user.createdEvents.push(event);
-                return user.save();
-            })
-            .then(result => {
-                return createdEvent;
-            })
-            .catch(err => {
-                console.log(err);
-                throw err;
-            });
-        },
-        createUser: args => {
-            // Logic to avoid storing single user multiple times
-            return User.findOne({
-                email: args.userInput.email
-            })
-            .then(user => {
-                if (user) {
-                    throw new Error('User exists already.')
-                }
-                return bcrypt.hash(args.userInput.password, 12)
-            })
-            // Logic for securely storing Passwords into Database
-            .then(hashedPassword => {
-                const user = new User({
-                    email: args.userInput.email, 
-                    password: hashedPassword
-                });
-                return user.save();
-            })
-            .then(result => {
-                return {...result._doc, password: null, _id: result.id};
-            })
-            .catch(err =>
-                {
-                    throw err;
-                });
-            
-        }
-    },
+    rootValue: graphQlResolvers,
     graphiql: true
 })
 );
